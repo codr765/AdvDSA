639f4ab17ea088cb05141dcb41bb02291015457a
codr765 <aryan852814@gmail.com>
2026-08-28 21:13:52 +0530

    p1

---
+++ TwoPointers/LongestPal.java
@@ -0,0 +1,14 @@
+class LongestPal {
+    static String longestPalindrome(String s) {
+        String pal = "";
+
+
+        return pal;
+    }
+
+    public static void main (String args[]) {
+        String str = "babad";
+
+        System.out.println(longestPalindrome(str));
+    }
+}
