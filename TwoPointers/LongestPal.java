9dc27cd82be8d92715610e894f014e6ae6f667a2
codr765 <aryan852814@gmail.com>
2026-08-29 19:56:54 +0530

    left right

--- TwoPointers/LongestPal.java
+++ TwoPointers/LongestPal.java
@@ -1,12 +1,35 @@
 class LongestPal {
-    static String longestPalindrome(String s) {
-        String pal = "";
 
+    static int expand(String s, int left, int right) {
 
-        return pal;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
     }
 
-    public static void main (String args[]) {
+    static String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public static void main(String args[]) {
         String str = "babad";
 
         System.out.println(longestPalindrome(str));
