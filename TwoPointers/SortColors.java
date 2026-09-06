425c1cba5a0e8a98d19188e54b80a1d42942a4d8
codr765 <aryan852814@gmail.com>
2026-09-06 13:58:58 +0530

    Sort colors

--- TwoPointers/SortColors.java
+++ TwoPointers/SortColors.java
@@ -1,8 +1,33 @@
+import java.util.Arrays;
+
 public class SortColors {
 
-    
+    static void sort(int[] colors) {
+        int left = -1;
+        int right = colors.length;
+        int mid = 0;
+
+        while (mid < right) {
+            if (colors[mid] == 0) {
+                int tmp = colors[mid];
+                colors[mid] = colors[++left];
+                colors[left] = tmp;
+                mid++;
+            } else if (colors[mid] == 2) {
+                int tmp = colors[mid];
+                colors[mid] = colors[--right];
+                colors[right] = tmp;
+            } else {
+                mid++;
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        int[] nums = { 2, 0, 2, 1, 1, 0 };
+        int[] colors = { 2, 0, 2, 1, 1, 0 };
+
+        sort(colors);
+
+        System.out.println(Arrays.toString(colors));
     }
 }
