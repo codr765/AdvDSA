f3d932b64902c9f4314a29ba588ad867ecb0a37e
codr765 <aryan852814@gmail.com>
2026-09-09 17:54:23 +0530

    maxavgsubarray

---
+++ SlidingWindow/MaxAvgSubarray.java
@@ -0,0 +1,41 @@
+public class MaxAvgSubarray {
+    static double findMaxAverage(int[] nums, int k) {
+        // double avg = 0;
+        // int sum = 0;
+
+        // for (int i = 0; i < k; i++) {
+        // sum += nums[i];
+        // }
+
+        // avg = (double) sum / k;
+
+        // for (int i = k; i < nums.length; i++) {
+        // sum += nums[i] - nums[i - k];
+        // avg = Math.max(avg, (double) sum / k);
+        // }
+
+        // return avg;
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return (double) maxSum / k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 12, -5, -6, 50, 3 };
+        int k = 4;
+
+        System.out.println(findMaxAverage(nums, k));
+    }
+}
