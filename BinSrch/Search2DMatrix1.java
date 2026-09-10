f70a0d758a505124235c26506667c75deb0356a5
codr765 <aryan852814@gmail.com>
2026-09-10 21:10:40 +0530

    2d array bin srch

---
+++ BinSrch/Search2DMatrix1.java
@@ -0,0 +1,37 @@
+public class Search2DMatrix1 {
+
+    static boolean searchMatrix(int[][] matrix, int target) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int start = 0;
+        int end = rows * cols - 1;
+        int mid = rows * cols / 2;
+
+        while (start <= end) {
+            int row = mid / matrix[0].length;
+            int col = mid % matrix[0].length;
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+
+            if (matrix[row][col] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            mid = (start + end) / 2;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+        int target = 3;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
