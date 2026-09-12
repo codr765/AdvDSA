eb924d273a33dac4c9494ce3d9c7d7e4662881b1
codr765 <aryan852814@gmail.com>
2026-09-12 16:44:42 +0530

    LC594

--- SlidingWindow/LC594.java
+++ SlidingWindow/LC594.java
@@ -1,23 +1,42 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LC594 {
 
     static int findLHS(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+        // Arrays.sort(nums);
 
-        int ans = 0;
+        // int count = 0;
+        // int left = 0;
 
-        for (int num : map.keySet()) {
-            if (map.containsKey(num + 1)) {
-                ans = Math.max(ans, map.get(num) + map.get(num + 1));
-            }
-        }
+        // for (int right = 0; right < nums.length; right++) {
+        //     while (nums[right] - nums[left] > 1) {
+        //         left++;
+        //     }
+        //     if (nums[right] - nums[left] == 1) {
+        //         count = Math.max(count, right - left + 1);
+        //     }
+        // }
 
-        return ans;
+        // return count;
+        
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // for (int num : nums) {
+        // map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+        // int ans = 0;
+
+        // for (int num : map.keySet()) {
+        // if (map.containsKey(num + 1)) {
+        // ans = Math.max(ans, map.get(num) + map.get(num + 1));
+        // }
+        // }
+
+        // return ans;
     }
 
     public static void main(String[] args) {
