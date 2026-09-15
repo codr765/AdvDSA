4a259c503ea79d29e40a89eca43b468f446e06a8
codr765 <aryan852814@gmail.com>
2026-09-15 19:48:12 +0530

    lc438

--- SlidingWindow/LC438.java
+++ SlidingWindow/LC438.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC438 {
-    static List<Integer> findAnagrams(String s, String p) {
+    static List<Integer> findAnagramsBad(String s, String p) {
         List<Integer> list = new ArrayList<>();
 
         int[] hash2 = new int[26];
@@ -39,10 +39,45 @@ public class LC438 {
         return list;
     }
 
+    static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+
+        int[] hash1 = new int[26];
+        int[] hash2 = new int[26];
+
+        for (int i = 0; i < p.length(); i++) {
+            hash2[p.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            hash1[s.charAt(i) - 'a']++;
+
+            if (i >= p.length()) {
+                hash1[s.charAt(i - p.length()) - 'a']--;
+            }
+
+            boolean chk = true;
+
+            for (int j = 0; j < 26; j++) {
+                if (hash1[j] != hash2[j]) {
+                    chk = false;
+                    break;
+                }
+            }
+
+            if (chk) {
+                list.add(i - p.length() + 1);
+            }
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         String s = "cbaebabacd";
         String p = "abc";
 
+        // System.out.println(findAnagramsBad(s, p));
         System.out.println(findAnagrams(s, p));
     }
 }
