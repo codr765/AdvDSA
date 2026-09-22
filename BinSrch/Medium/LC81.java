c3e7963acfe7cfccea557bb2bcdf0b422a2f5911
codr765 <aryan852814@gmail.com>
2026-09-22 21:42:31 +0530

    lc81

---
+++ BinSrch/Medium/LC81.java
@@ -0,0 +1,39 @@
+public class LC81 {
+    static boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 5, 6, 0, 0, 1, 2 };
+
+        System.out.println(search(nums, 0));
+    }
+}
