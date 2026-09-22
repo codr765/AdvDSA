5fa22d54b2861d073bfd14afea1f5ca270a469ca
codr765 <aryan852814@gmail.com>
2026-09-22 20:58:25 +0530

    lc33

---
+++ BinSrch/Medium/LC33.java
@@ -0,0 +1,37 @@
+public class LC33 {
+
+    static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 5, 6, 7, 8, 0, 1, 2 };
+
+        System.out.println(search(nums, 4));
+    }
+}
