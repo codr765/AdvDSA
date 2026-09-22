e8e75ec06bb42e8b71a07a5c8f35395ff02b4a0a
codr765 <aryan852814@gmail.com>
2026-09-22 20:30:22 +0530

    lc34

---
+++ BinSrch/Easy/LC704.java
@@ -0,0 +1,28 @@
+public class LC704 {
+    static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 3, 5, 9, 12 };
+
+        System.out.println(search(nums, 9));
+    }
+}
---
+++ BinSrch/Medium/LC34.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class LC34 {
+    static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[] { first, last };
+    }
+
+    static int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 7, 7, 8, 8, 10 };
+
+        System.out.println(Arrays.toString(searchRange(nums, 8)));
+    }
+}
